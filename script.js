3a86d9296e374cfad0f10c604ef2510a77dece18
AhmadAllan <aaa200265@gmail.com>
2023-03-27 11:05:57 +0300

    some changes

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const form = document.querySelector('.top-banner form');
 const input = document.querySelector('.top-banner input');
 const msg = document.querySelector('.top-banner .msg');
 const list = document.querySelector('.ajax-section .cities');
+
 const defaultCities = ['jordan', 'Irbid', 'Ajloun', 'Jarash', 'Mafraq', 'Madaba', 'Zarqa', 'Amman', 'Salt', 'Karak', 'Aqaba'];
 
 
 // Function to check if city is already in the list
-function isCityInList(cityName, cityList) {
-  const listItemsArray = Array.from(cityList.querySelectorAll('.city'));
+function isCityInList(cityName) {
+  const listItemsArray = Array.from(document.querySelectorAll('.city'));
   const filteredArray = listItemsArray.filter((el) => {
     const content = el.querySelector('.city-name span').textContent.toLowerCase();
     return content === cityName.toLowerCase();
@@ -18,7 +19,7 @@ function isCityInList(cityName, cityList) {
 }
 
 // Function to fetch weather data and add to the list
-function addCityToList(cityName, cityList, msgElement) {
+function addCityToList(cityName, msg) {
   const apiKey = 'f371df7782ef6053effdc1ebc0565ec8';
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
   fetch(url)
@@ -40,41 +41,41 @@ function addCityToList(cityName, cityList, msgElement) {
         </figure>
       `;
       li.innerHTML = markup;
-      cityList.appendChild(li);
+      list.appendChild(li);
     })
     .catch(() => {
-      msgElement.textContent = 'Please search for a valid city';
+      msg.textContent = 'Please search for a valid city';
     });
 }
 
 // Function to handle form submission
-function handleFormSubmit(e, inputElement, cityList, msgElement) {
+function handleFormSubmit(e, inputElement, msg) {
   e.preventDefault();
   const cityName = inputElement.value;
-  if (isCityInList(cityName, cityList)) {
-    msgElement.textContent = `${cityName} is already in the list`;
+  if (isCityInList(cityName)) {
+    msg.textContent = `${cityName} is already in the list`;
   } else {
-    addCityToList(cityName, cityList, msgElement);
-    msgElement.textContent = '';
+    addCityToList(cityName, msg);
+    msg.textContent = '';
     inputElement.value = '';
     inputElement.focus();
   }
 }
 
 // Function to add default cities to the list
-function addDefaultCities(cityNames, cityList, msgElement) {
+function addDefaultCities(cityNames, msg) {
   cityNames.forEach((cityName) => {
-    addCityToList(cityName, cityList, msgElement);
+    addCityToList(cityName, msg);
   });
 }
 
 // Function to reset the form and remove all cities from the list
-form.addEventListener('submit', (e) => handleFormSubmit(e, input, list, msg));
+form.addEventListener('submit', (e) => handleFormSubmit(e, input, msg));
 document.getElementById("reset").addEventListener("click", () => {
   list.innerHTML = ""; // removes all the li elements inside the ul element
 });
 
 
 window.onload = () => {
-  addDefaultCities(defaultCities, list, msg);
+  addDefaultCities(defaultCities, msg);
 }
